7547f36c7b6db5bc57e627f70e74b149dd5962b5
Craig <craigyazzie143@icloud.com>
2026-05-07 12:17:56 -0600

    Update index.js
    gg

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let firstName = "Craig";
 let jobTitle = "Developer";
 let city = "Phoenix";
 
-let newJobPost = '${firstName} is a ${jobTitle} in ${city}."';
+let newJobPost = `${firstName} is a ${jobTitle} in ${city}.`;
 document.getElementById('result').innerHTML = newJobPost;
