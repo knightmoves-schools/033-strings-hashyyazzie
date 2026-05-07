1a6c89c4bf16fbecbc38e167eb01cc2825126a54
Craig <craigyazzie143@icloud.com>
2026-05-07 12:16:34 -0600

    Update index.js
    h

---
+++ index.js
@@ -0,0 +1,6 @@
+let firstName = "Craig";
+let jobTitle = "Developer";
+let city = "Phoenix";
+
+let newJobPost = '${firstName} is a ${jobTitle} in ${city}."';
+document.getElementById('result').innerHTML = newJobPost;
